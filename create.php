6aae54e04958f21e47ecdacb8a54783121378fc2
maxam2017 <whk0313@gmail.com>
2018-01-08 01:27:30 +0800

    change some layout

--- create.php
+++ create.php
@@ -34,16 +34,16 @@ if(isset($_POST['submit'])){
   $user->post($title,$content,$_SESSION['user_session']);
 }
 ?>
-                                          <head>
-                                            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-                                            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-                                            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-                                            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-                                            <link href="asset/css/regi.css" rel="stylesheet">
-                                          </head>
-                                          <body>
-                                            <?php require 'header.php';?>
-                                            <div id="container2">
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  <link href="asset/css/regi.css" rel="stylesheet">
+</head>
+<body>
+  <?php require 'header.php';?>
+  <div id="container2">
 <?php
 //if(isset($_POST['submit'])){
 ?>
@@ -55,16 +55,48 @@ if(isset($_POST['submit'])){
 <?php
 //}
 ?>
-                                                         <fieldset><legend><h3>管理留言</h3></legend></fieldset>
-                                                         <hr>
-                                                         <table class="table table-dark table-striped text-center table-hover my-4">
-                                                           <tbody>
-                                                             <tr>
-                                                               <th>編號</th>
-                                                               <th>標題</th>
-                                                              <th>內容</th> 
-                                                               <th>管理</th>
-                                                             </tr>
+
+<fieldset><legend><h3>新增留言</h3></legend></fieldset>
+<hr>
+<form action="create.php" method="post" enctype="multipart/form-data">
+  <div class="form-group row ">
+    <label class="col-sm-2 col-form-label">標題</label>
+    <div class="col-sm-10">
+      <input  class="form-control" name="title" maxlength="10" placeholder="Title" required>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-sm-2 col-form-label">內容</label>
+    <div class="col-sm-10">
+      <textarea class="form-control" maxlength="1000" name="content" placeholder="最多輸入1000字" rows="8" required></textarea>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <div class="form-check">
+        <label class="form-check-label">
+          <input class="form-check-input" type="checkbox" required>確認是否發文 
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-12">
+      <a class="btn btn-secondary" href="index.php">返回</a>
+      <button type="submit" name="submit" class="btn btn-primary mx-2">發佈貼文</button>
+    </div>
+  </div>
+</form>
+<fieldset><legend><h3>管理留言</h3></legend></fieldset>
+<hr>
+<table class="table table-dark table-striped text-center table-hover my-4">
+  <tbody>
+    <tr>
+      <th>編號</th>
+      <th>標題</th>
+      <th>內容</th> 
+      <th>管理</th>
+    </tr>
 <?php
 $sql = 'SELECT * FROM post where user_id='.$_SESSION['user_session'].' ORDER BY id DESC';
 $q = $DB_con->prepare($sql);
@@ -83,40 +115,9 @@ foreach ($q as $row) {
   $t++;
 }
 ?>
-                                                           </tbody>
-                                                         </table>
-                                                         <fieldset><legend><h3>新增留言</h3></legend></fieldset>
-                                                         <hr>
-                                                         <form action="create.php" method="post" enctype="multipart/form-data">
-                                                           <div class="form-group row ">
-                                                             <label class="col-sm-2 col-form-label">標題</label>
-                                                             <div class="col-sm-10">
-                                                               <input  class="form-control" name="title" maxlength="10" placeholder="Title" required>
-                                                             </div>
-                                                           </div>
-                                                           <div class="form-group row">
-                                                             <label class="col-sm-2 col-form-label">內容</label>
-                                                             <div class="col-sm-10">
-                                                               <textarea class="form-control" maxlength="1000" name="content" placeholder="最多輸入1000字" rows="8" required></textarea>
-                                                             </div>
-                                                           </div>
-                                                           <div class="form-group row">
-                                                             <div class="col-sm-10">
-                                                               <div class="form-check">
-                                                                 <label class="form-check-label">
-                                                                   <input class="form-check-input" type="checkbox" required>確認是否發文 
-                                                                 </label>
-                                                               </div>
-                                                             </div>
-                                                           </div>
-                                                           <div class="form-group row">
-                                                             <div class="col-sm-12">
-                                                               <a class="btn btn-secondary" href="index.php">返回</a>
-                                                               <button type="submit" name="submit" class="btn btn-primary mx-2">發佈貼文</button>
-                                                             </div>
-                                                           </div>
-                                                         </form>
-                                            </div>
-                                            <script src="asset/js/main.js"></script>
-                                          </body>
+  </tbody>
+</table>
+  </div>
+  <script src="asset/js/main.js"></script>
+</body>
 
